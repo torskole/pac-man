2ef9c427eaf34e3c03ad9c0eef5babbec6e3be6f
torskole <tor2205@skole.vtfk.no>
2024-03-12 14:19:00 +0100

    Fuctional movement (?)

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import pygame
 from pygame.locals import (K_UP, K_DOWN, K_LEFT, K_RIGHT, K_w, K_s, K_a, K_d)
+from config import *
 
 pygame.init()
 
-WINDOW_WIDTH = 500
-WINDOW_HEIGHT = 500
+width = 25
+height = 25
+blocksize = 25
+
+WINDOW_WIDTH = width*blocksize
+WINDOW_HEIGHT = height*blocksize
 
 surface = pygame.display.set_mode([WINDOW_WIDTH, WINDOW_HEIGHT])
 
@@ -12,36 +17,80 @@ clock = pygame.time.Clock()
 maze = pygame.image.load("maze.png").convert_alpha()
 maze = pygame.transform.scale(maze, (WINDOW_WIDTH, WINDOW_HEIGHT))
 
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-
 class Game:
     def __init__(self) -> None:
         self.current_score = 0
         self.current_level = 1
+        self.coordinates = []
+        for column in range(width):
+            self.coordinates.append([])
+            for _ in range(0, height):
+                self.coordinates[column].append({"type": None, "color": WHITE})
 
-class Pacman:
-    def __init__(self, x, y) -> None:
-        self.x = x
-        self.y = y
+    def render_grid(self):
+        for x in range(0, len(self.coordinates)):
+            for y in range (0, len(self.coordinates[x])):
+                pygame.draw.rect(surface, self.coordinates[x][y]["color"], pygame.Rect(blocksize*x, blocksize*y, blocksize, blocksize), 10)
 
-    def move(self, keys):
-        if keys[K_UP] or keys[K_w]: # Upwards
-            self.y -= 1
-        if keys[K_DOWN] or keys[K_s]: # Downwards
-            self.y += 1
-        if keys[K_LEFT] or keys[K_a]: # Left
-            self.x -= 1
-        if keys[K_RIGHT] or keys[K_d]: # Right
-            self.x += 1
+class MovingFigure:
+    def __init__(self, game, x, y, sprite) -> None:
+        self.game = game
+        self.current_position = {"x": x, "y": y}
+        self.new_position = [0, 0]
+        self.direction = {"x": 0, "y": 0}
+        self.sprite = sprite
 
-    def render_pacman(self, keys):
-        self.move(keys)
-        pygame.draw.rect(surface, WHITE, pygame.Rect(self.x, self.y, 20, 20))
+class Ghost(MovingFigure):
+    def __init__(self, game, x, y, sprite) -> None:
+        super().__init__(game, x, y, sprite)
+
+class Pacman(MovingFigure):
+    def __init__(self, game, x, y, sprite) -> None:
+        super().__init__(game, x, y, sprite)
+        self.health = 2
         
+    def move(self):
+        if self.direction["y"] == 1:
+            self.new_position[1] = self.current_position["y"] - 1
+        elif self.direction["y"] == -1:
+            self.new_position[1] = self.current_position["y"] + 1
+        if self.direction["x"] == 1:
+            self.new_position[0] = self.current_position["x"] + 1
+        elif self.direction["x"] == -1:
+            self.new_position[0] = self.current_position["x"] - 1
+
+        if self.current_position == self.new_position:
+            print(True)
+        else:
+            print(False)
+        #print(self.current_position, self.new_position)
+
+        game.coordinates[self.current_position["x"]][self.current_position["y"]]["color"] = WHITE
+        game.coordinates[self.new_position[0]][self.new_position[1]]["color"] = YELLOW
+
+        self.current_position["x"] = self.new_position[0]
+        self.current_position["y"] = self.new_position[1]
+
+        self.direction["x"] = 0
+        self.direction["y"] = 0
+
+    def queue_movement(self, keys):
+        if keys[K_UP] or keys[K_w]: # Upwards
+            self.direction["y"] = 1
+        if keys[K_DOWN] or keys[K_s]: # Downwards
+            self.direction["y"] = -1
+        if keys[K_LEFT] or keys[K_a]: # Left
+            self.direction["x"] = -1
+        if keys[K_RIGHT] or keys[K_d]: # Right
+            self.direction["x"] = 1
+
+
 active = True
 
-active_pacman = Pacman(0, 0)
+game = Game()
+
+active_pacman = Pacman(game, 0, 0, None)
+frames = 0
 
 while active:
     for event in pygame.event.get():
@@ -56,11 +105,16 @@ while active:
 
     keys = pygame.key.get_pressed()
 
-    surface.fill(WHITE)
-    surface.blit(maze, (0, 0))
-    active_pacman.render_pacman(keys)
+    surface.fill(BLACK)
+    #surface.blit(maze, (0, 0))
+    active_pacman.queue_movement(keys)
 
+    if frames % CHANGE_INTERVAL == 0:
+        active_pacman.move()
+
+    game.render_grid()
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(FPS)
+    frames += 1
 
 pygame.quit()
